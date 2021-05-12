f951a6484bc8fa6951d30aa1d85e35eefdc62e29
Deivid <deivid.magno.nascimento@gmail.com>
2021-05-12 19:59:14 -0300

    Logica das operações

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default class App extends Component {
   state = {...initialState}
 
   addDigit = n => {
-    if(n == '.' && this.state.displayValue.includes('.')){
+    if(n == '.' && !clearDisplay && this.state.displayValue.includes('.')){
       return
     }
 
@@ -38,7 +38,25 @@ export default class App extends Component {
   }
 
   setOperation = operation => {
-
+    if(this.state.current === 0){
+      this.setState({ operation, current: 1, clearDisplay: true })
+    }else{
+      const equals = operation === '='
+      const values = [...this.state.values]
+      try{
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      }catch(e){
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values,
+      })
+    }
   }
 
   render(){
@@ -47,22 +65,22 @@ export default class App extends Component {
         <Display value={this.state.displayValue}/>
         <View style={styles.buttons}>
           <Button label='AC' triple onClick={this.clearMemory}/>
-          <Button label='/'operation onClick={this.seOperation} />
+          <Button label='/'operation onClick={this.setOperation} />
           <Button label='7'onClick={this.addDigit}/>
           <Button label='8'onClick={this.addDigit}/>
           <Button label='9'onClick={this.addDigit}/>
-          <Button label='*'operation onClick={this.seOperation}/>
+          <Button label='*'operation onClick={this.setOperation}/>
           <Button label='4'onClick={this.addDigit}/>
           <Button label='5'onClick={this.addDigit}/>
           <Button label='6'onClick={this.addDigit}/>
-          <Button label='-' operation onClick={this.seOperation}/>
+          <Button label='-' operation onClick={this.setOperation}/>
           <Button label='1'onClick={this.addDigit}/>
           <Button label='2'onClick={this.addDigit}/>
           <Button label='3'onClick={this.addDigit}/>
-          <Button label='+'operation onClick={this.seOperation}/>
+          <Button label='+'operation onClick={this.setOperation}/>
           <Button label='0'double onClick={this.addDigit}/>
           <Button label='.'onClick={this.addDigit}/>
-          <Button label='='operation onClick={this.seOperation}/>
+          <Button label='='operation onClick={this.setOperation}/>
         </View>
       </View>
     );
